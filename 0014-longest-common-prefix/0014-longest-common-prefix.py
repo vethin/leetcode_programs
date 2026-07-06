1f880fc067a038172b2c797701dcdb2c7248aac6
vethin <bandivethin@gmail.com>
2026-07-06 23:11:11 +0530

    Time: 0 ms (100%), Space: 12.4 MB (33.05%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        prefix = strs[0]
+
+        for word in strs[1:]:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+
+        return prefix
